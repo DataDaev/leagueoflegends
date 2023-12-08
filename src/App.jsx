e1d969f9a0b29a48c396a0bfc5dc945e9b60f3b9
DataDaev <Datadaev@gmail.com>
2023-12-08 08:26:07 -0700

    added background video

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import BackgroundVideo from "./video-background";
+
+export default function App() {
+  return (
+    <div>
+      <BackgroundVideo />
+      <div className="content">Content 2</div>
+    </div>
+  );
+}
