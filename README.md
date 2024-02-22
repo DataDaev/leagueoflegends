f00f1175776fb5f5f9cb346c8fde95eb0937fd11
DataDaev <Datadaev@gmail.com>
2024-02-21 17:59:23 -0700

    removed mp4 and replaced with poster jpg for Kindred

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# leagueoflegends
+# Lolchamp
+
+# League of Legends website: Lolchamp
+
+Lolchamp is a website for users to search the biography
+and skills of every league of legends champion.
+
+Submitted by: <strong>Davis Tran</strong>
+
+# Website Walkthrough
--- src/VideoBackground.jsx
+++ src/VideoBackground.jsx
@@ -1,11 +1,10 @@
 import backgroundvideoWebm from "./assets/animated-kindred.webm";
-import backgroundvideoMP4 from "./assets/animated-kindred.mp4";
+import videoPoster from "./assets/Kindred.jpg";
 
 export default function BackgroundVideo() {
   return (
     <div className="background-video">
-      <video autoPlay loop muted className="video">
-        <source src={backgroundvideoMP4} type="video/mp4" />
+      <video autoPlay loop muted className="video" poster={videoPoster}>
         <source src={backgroundvideoWebm} type="video/webm" />
         Your browser does not support the video tag.
       </video>
