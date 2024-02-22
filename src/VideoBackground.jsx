80a1e41b1047460d4dd90b6f6ba85f5a9508956c
DataDaev <Datadaev@gmail.com>
2024-02-21 17:30:46 -0700

    media queries for lore-skills and added mp4 of kindred video

--- src/VideoBackground.jsx
+++ src/VideoBackground.jsx
@@ -1,11 +1,13 @@
-import backgroundvideo from "./assets/animated-kindred.webm";
+import backgroundvideoWebm from "./assets/animated-kindred.webm";
+import backgroundvideoMP4 from "./assets/animated-kindred.mp4";
 
 export default function BackgroundVideo() {
   return (
     <div className="background-video">
-      <video autoPlay loop muted src={backgroundvideo} className="video">
-        {/* <source type="video/webm" />
-        Your browser does not support the video tag. */}
+      <video autoPlay loop muted className="video">
+        <source src={backgroundvideoMP4} type="video/mp4" />
+        <source src={backgroundvideoWebm} type="video/webm" />
+        Your browser does not support the video tag.
       </video>
       <div className="overlay"></div>
     </div>
